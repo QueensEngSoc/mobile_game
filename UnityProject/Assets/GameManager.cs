f49eef37970adde00c4fdbf72ffee610d2a99c62
BluBloos <cnoah1705@gmail.com>
2019-10-31 18:30:56 -0400

    Added GameManager.cs and Node.cs. These define the spec for the saving and loading of a vehicle description

---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
+[Serializable]
+public class vehicle_desc
+{
+    List<Node> nodes;
+    List<NodeConnection> connections;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject vehiclePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vehicle newVehicle = Instantiate(vehiclePrefab, Vector3.zero, Quaternion.identity).GetComponent<Vehicle>();
+        //newVehicle.LoadVehicle("resource");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SaveVehicle(vehicle_desc vehicle, string fileName)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/" + fileName, FileMode.Create);
+        bf.Serialize(stream, vehicle);
+        stream.Close();
+    }
+
+    public vehicle_desc LoadVehicle(string fileName)
+    {
+        string filePath = Application.persistentDataPath + "/" + fileName;
+
+        if (File.Exists(filePath))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+            vehicle_desc desc = (vehicle_desc)bf.Deserialize(stream);
+            return desc;
+        }
+
+        Debug.Log("Hey dude, it didn't load.");
+
+        return null;
+    }
+}
---
+++ UnityProject/Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum node_type
+{
+    NODE,
+    STATIC_NODE // laws of physics don't apply, literally just fixed in place.
+}
+
+public class Node
+{
+    public node_type nodeType;
+    public PhysicsBody physicsBody;
+    public PseudoPosition pseudoPosition;
+
+    public Node()
+    {
+        pseudoPosition = new PseudoPosition();
+        physicsBody = new PhysicsBody(pseudoPosition);
+        
+    }
+
+    List<NodeAttachment> attachments;    
+}
+
+public class NodeConnection
+{
+    int nodeIndex1;
+    int nodeIndex2;
+}
+
+public enum node_attachment_type
+{
+    NODE_ATTACHMENT_WHEEL,
+    NODE_ATTACHMENT_PISTON
+}
+
+public class NodeAttachment
+{
+    node_attachment_type attachmentType;
+    Vector3 sphereNormal;
+}
